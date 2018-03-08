33d3754427332a504974ddf8c4c48af8b0a6bb19
root <root@DESKTOP-TP7CE4Q.localdomain>
2018-03-08 15:34:28 +1000

    got alot of the reading working, need to find my own IP address or host. confirm the ipaddr that we currently have

--- a1pc.py
+++ a1pc.py
@@ -1,11 +1,35 @@
 #Assignment 1 Part C
 
 import socket
-request = b"GET / HTTP/1.1\nHost: www.google.com\n\n"
+
+
+#print header to assignment
+print(b"HTTP Protocol Analyzer, Written by <Thomas Evetts>, <43529610>")
+
+#print(b"\r\nURL Requested:")
+input_request = input(b"\r\nURL Requested:")
+input_request = input_request.encode('utf-8')
+
+print(input_request[2:])
+
+#append the get / http.....
+request = b"GET / HTTP/1.1\nHost: " + input_request + b"\n\n"
+print(request)
+#get the request from the user
+
+
+#request = b"GET / HTTP/1.1\nHost: www.google.com\n\n" #get this from cmd line
+#socket setup
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("google.com", 80))
+s.connect((input_request[4:], 80))
+serveripaddr = s.getsockname()[0].encode('utf-8')
+print(b"IP Address, Port of the Server: " + serveripaddr + b" ,80")
+
 s.send(request)
+
+#process the response from the website
 result = s.recv(10000)
 while (len(result) > 0):
     print(result)
     result = s.recv(10000)
+#chase process if result contains a response other than 200 OK
