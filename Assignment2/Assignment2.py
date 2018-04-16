5c6961ce414737a50c3001be1fb77cfa58d4c257
root <root@DESKTOP-TP7CE4Q.localdomain>
2018-04-16 20:14:58 +1000

    got fundamentals working

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -10,7 +10,7 @@ def send_udp_message(message, address, port):
     message = message.replace(" ", "").replace("\n", "")
     server_address = (address, port)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # AF_INET6
     try:
         sock.sendto(binascii.unhexlify(message), server_address)
         data, _ = sock.recvfrom(4096)
@@ -29,8 +29,21 @@ def format_hex(hex):
 message = "AA AA 01 00 00 01 00 00 00 00 00 00 " \
 "07 65 78 61 6d 70 6c 65 03 63 6f 6d 00 00 01 00 01"
 
-message_ipaddr = "AA AA 09 00 00 01 00 00 00 00 00 00 " \
-"04 5d b8 d8 22 00 00 01 00 01"
+# "AA AA 09 00 00 01 00 00 00 00 00 00 " \
+message_ipaddr = "00 01 01 00 00 01 00 00 00 00 00 00" \
+                 "04 82 66 47 a0 00 00 01 00 01"
 
-response = send_udp_message(message_ipaddr, "8.8.8.8", 53)
+# this one works bois for inverse request
+message_ipaddr_rev = "d6 f8 01 00 00 01" \
+                     "00 00 00 00 00 00 03 31 36 30 02 37 31 03 31 30" \
+                     "32 03 31 33 30 07 69 6e 2d 61 64 64 72 04 61 72" \
+                     "70 61 00 00 0c 00 01"
+
+message_2 = "00 01 01 00 00 01 00 00  00 00 00 00 03 77 77 77" \
+            "06 67 6f 6f 67 6c 65 03  63 6f 6d 00 00 01 00 01"
+
+response = send_udp_message(message, "8.8.8.8", 53) # "2001:4860:4860::8888" ipv6 server
+
+#process the input from a given string
+
 print(format_hex(response))
